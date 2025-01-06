4325c3a260a186e903af55d4279731e08b4b767c
Gyuwon Lee <gyuwon@kaist.ac.kr>
2025-01-06 14:55:14 +0900

    홈에 스크롤뷰 추가

--- frontend/app/(auth)/(tabs)/home.tsx
+++ frontend/app/(auth)/(tabs)/home.tsx
@@ -1,4 +1,4 @@
-import { Button, Text, View } from "react-native";
+import { Button, ScrollView, Text, View } from "react-native";
 import { router } from "expo-router";
 import React from "react";
 import { sharedStyles } from "@/app/_layout";
@@ -12,35 +12,171 @@ export default function HomeScreen() {
         style={[
           sharedStyles.container,
           sharedStyles.contentCentered,
-          sharedStyles.horizontalPadding,
         ]}
       >
-        <Text>Home Screen</Text>
-        <Button
-          title="Go to Notification"
-          onPress={() => router.push("/notification")}
-        />
-        <Button
-          title="Go to Settings"
-          onPress={() => router.push("/settings")}
-        />
-        <Button
-          title="Go to Profile"
-          onPress={() => router.push("/profiles")}
-        />
-        <Button title="Go to Search" onPress={() => router.push("/search")} />
-        <Button
-          title="Go to Messages Inbox"
-          onPress={() => router.push("/conversations")}
-        />
-        <Button
-          title="Go to Login"
-          onPress={() => router.push("/onboarding")}
-        />
-        <Button
-          title="Go to Signup"
-          onPress={() => router.push("/signupOnboarding")}
-        />
+        <ScrollView style={{ 
+          width: "100%", 
+          borderTopWidth: 4,
+          borderColor: '#F5F5F5',
+          padding: 16,
+        }}>
+          <Text>Home Screen</Text>
+          <Button
+            title="Go to Notification"
+            onPress={() => router.push("/notification")}
+          />
+          <Button
+            title="Go to Settings"
+            onPress={() => router.push("/settings")}
+          />
+          <Button
+            title="Go to Profile"
+            onPress={() => router.push("/profiles")}
+          />
+          <Button title="Go to Search" onPress={() => router.push("/search")} />
+          <Button
+            title="Go to Messages Inbox"
+            onPress={() => router.push("/conversations")}
+          />
+          <Button
+            title="Go to Login"
+            onPress={() => router.push("/onboarding")}
+          />
+          <Button
+            title="Go to Signup"
+            onPress={() => router.push("/signupOnboarding")}
+          />
+          <Text>Home Screen</Text>
+          <Button
+            title="Go to Notification"
+            onPress={() => router.push("/notification")}
+          />
+          <Button
+            title="Go to Settings"
+            onPress={() => router.push("/settings")}
+          />
+          <Button
+            title="Go to Profile"
+            onPress={() => router.push("/profiles")}
+          />
+          <Button title="Go to Search" onPress={() => router.push("/search")} />
+          <Button
+            title="Go to Messages Inbox"
+            onPress={() => router.push("/conversations")}
+          />
+          <Button
+            title="Go to Login"
+            onPress={() => router.push("/onboarding")}
+          />
+          <Button
+            title="Go to Signup"
+            onPress={() => router.push("/signupOnboarding")}
+          />
+          <Text>Home Screen</Text>
+          <Button
+            title="Go to Notification"
+            onPress={() => router.push("/notification")}
+          />
+          <Button
+            title="Go to Settings"
+            onPress={() => router.push("/settings")}
+          />
+          <Button
+            title="Go to Profile"
+            onPress={() => router.push("/profiles")}
+          />
+          <Button title="Go to Search" onPress={() => router.push("/search")} />
+          <Button
+            title="Go to Messages Inbox"
+            onPress={() => router.push("/conversations")}
+          />
+          <Button
+            title="Go to Login"
+            onPress={() => router.push("/onboarding")}
+          />
+          <Button
+            title="Go to Signup"
+            onPress={() => router.push("/signupOnboarding")}
+          />
+          <Text>Home Screen</Text>
+          <Button
+            title="Go to Notification"
+            onPress={() => router.push("/notification")}
+          />
+          <Button
+            title="Go to Settings"
+            onPress={() => router.push("/settings")}
+          />
+          <Button
+            title="Go to Profile"
+            onPress={() => router.push("/profiles")}
+          />
+          <Button title="Go to Search" onPress={() => router.push("/search")} />
+          <Button
+            title="Go to Messages Inbox"
+            onPress={() => router.push("/conversations")}
+          />
+          <Button
+            title="Go to Login"
+            onPress={() => router.push("/onboarding")}
+          />
+          <Button
+            title="Go to Signup"
+            onPress={() => router.push("/signupOnboarding")}
+          />
+          <Text>Home Screen</Text>
+          <Button
+            title="Go to Notification"
+            onPress={() => router.push("/notification")}
+          />
+          <Button
+            title="Go to Settings"
+            onPress={() => router.push("/settings")}
+          />
+          <Button
+            title="Go to Profile"
+            onPress={() => router.push("/profiles")}
+          />
+          <Button title="Go to Search" onPress={() => router.push("/search")} />
+          <Button
+            title="Go to Messages Inbox"
+            onPress={() => router.push("/conversations")}
+          />
+          <Button
+            title="Go to Login"
+            onPress={() => router.push("/onboarding")}
+          />
+          <Button
+            title="Go to Signup"
+            onPress={() => router.push("/signupOnboarding")}
+          />
+          <Text>Home Screen</Text>
+          <Button
+            title="Go to Notification"
+            onPress={() => router.push("/notification")}
+          />
+          <Button
+            title="Go to Settings"
+            onPress={() => router.push("/settings")}
+          />
+          <Button
+            title="Go to Profile"
+            onPress={() => router.push("/profiles")}
+          />
+          <Button title="Go to Search" onPress={() => router.push("/search")} />
+          <Button
+            title="Go to Messages Inbox"
+            onPress={() => router.push("/conversations")}
+          />
+          <Button
+            title="Go to Login"
+            onPress={() => router.push("/onboarding")}
+          />
+          <Button
+            title="Go to Signup"
+            onPress={() => router.push("/signupOnboarding")}
+          />
+        </ScrollView>
       </View>
     </>
   );
