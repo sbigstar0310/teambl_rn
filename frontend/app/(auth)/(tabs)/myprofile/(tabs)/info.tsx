639f602b7a074fea3aa93d34b17201e960457f53
KingsFavor <rnjsdn1003@kaist.ac.kr>
2025-01-12 18:39:08 +0900

    관심사 입력 컴포넌트

--- frontend/app/(auth)/(tabs)/myprofile/(tabs)/info.tsx
+++ frontend/app/(auth)/(tabs)/myprofile/(tabs)/info.tsx
@@ -1,11 +1,12 @@
 import DegreeBottomModal from '@/components/DegreeBottomModal';
+import KeywordInput from '@/components/KeywordInput';
 import theme from '@/shared/styles/theme';
-import React from 'react';
+import React, { useState } from 'react';
 import { ScrollView, View, Text, StyleSheet } from 'react-native';
 
 const MyProfileInfoView = () => {
 
-    const [isSkillModalVisible, setIsSkillModalVisible] = React.useState(false);
+    const [currentSkillList, setCurrentSkillList] = useState(["관심사1", "관심사4", "관심사5", "긴이름을가진관심사"]);
 
     return (
         <ScrollView
@@ -14,21 +15,23 @@ const MyProfileInfoView = () => {
             <View
                 style={styles.innerContainer}
             >
-                <Text
-                    onPress={() => {
-                        setIsSkillModalVisible(true);
-                    }}
+                <View
+                    style={styles.fieldTitleContainer}
                 >
-                    Info!
-                </Text>
-               <DegreeBottomModal
-                    visible={isSkillModalVisible}
-                    onClose={() => {
-                        setIsSkillModalVisible(false);
-                    }}
-                    handleDegreeSelect={() => {}}
-                    selectedDegree={"학사"}
-                    heightPercentage={0.3}
+                    <Text
+                        style={styles.fieldTitle}
+                    >
+                        {"관심사"}
+                    </Text>
+                    <Text
+                        style={styles.fieldSubTitle}
+                    >
+                        {"최대 5개"}
+                    </Text>
+                </View>
+                <KeywordInput
+                    currentKeyworldList={currentSkillList}
+                    setCurrentKeywordList={setCurrentSkillList}
                 />
             </View>
         </ScrollView>
@@ -43,11 +46,33 @@ const styles = StyleSheet.create({
     },
     innerContainer: {
         flex: 1,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
         backgroundColor: theme.colors.white,
         paddingTop: 15,
         paddingBottom: 50,
         paddingHorizontal: 15
     },
+    fieldTitleContainer : {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        paddingBottom: 12
+    },
+    fieldTitle : {
+        fontSize: theme.fontSizes.subtitle,
+        fontWeight: 600,
+        color: theme.colors.black,
+        marginRight: 12
+    },
+    fieldSubTitle : {
+        fontSize: theme.fontSizes.body2,
+        fontWeight: 400,
+        color: theme.colors.achromatic01
+    }
 });
 
 export default MyProfileInfoView;
---
+++ frontend/components/KeywordInput.tsx
@@ -0,0 +1,195 @@
+import theme from '@/shared/styles/theme';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View, TextInput } from 'react-native';
+import DeleteIcon from '@/assets/delete-x-icon.svg';
+
+const KeywordBadge = (props: any) => {
+
+    const {
+        keyword,
+        isFixed,
+        value,
+        onChange,
+        onDelete,
+        handleAddKeyword
+    } = props;
+
+    if (isFixed) {
+        return (
+            <View
+                style={styles.badgeContainer}
+            >
+                <Text
+                    style={styles.badgeText}
+                >
+                    {keyword}
+                </Text>
+                <TouchableOpacity
+                    onPress={onDelete}
+                >
+                    <DeleteIcon />
+                </TouchableOpacity>
+            </View>
+        );
+    } else {
+        /** adding new one */
+        return (
+            <View
+                style={[styles.badgeContainer]}
+            >
+                <TextInput
+                    style={[styles.badgeTextInput]}
+                    value={value}
+                    onChangeText={onChange}
+                    onSubmitEditing={handleAddKeyword}
+                    onBlur={handleAddKeyword}
+                    placeholder='새 관심사를 입력해주세요'
+                    placeholderTextColor={theme.colors.achromatic04}
+                />
+            </View>
+        );
+    }
+};
+
+const KeywordInput = (props: any) => {
+
+    const {
+        currentKeyworldList,
+        setCurrentKeywordList,
+        placeholderText = "본인을 나타내는 관심사를 입력해보세요",
+        maxNumber = 5
+    } = props;
+
+    const [newKeyword, setNewKeyword] = useState("");
+    const [isKeywordInputOnFocus, setIsKeywordInputOnFocus] = useState(false);
+
+    const handleOnFocus = () => {
+        setIsKeywordInputOnFocus(true);
+    };
+
+    const handleAddKeyword = () => {
+        if (newKeyword.trim()) {
+            setCurrentKeywordList((prevState: any) => [...prevState, newKeyword.trim()]);
+            setNewKeyword("");
+        }
+    };
+
+    const deleteKeyword = (index: number) => {
+        setCurrentKeywordList((prevState: any) => {
+            let newState = [...prevState];
+            newState.splice(index, 1);
+            return newState;
+        });
+    }
+
+    useEffect(() => {
+        if (currentKeyworldList.length === 0) {
+            setIsKeywordInputOnFocus(false);
+        } else {
+            setIsKeywordInputOnFocus(true);
+        }
+    }, [currentKeyworldList]);
+
+    return (
+        <View
+            style={styles.container}
+        >
+            {
+                (currentKeyworldList.length === 0) &&
+                (!isKeywordInputOnFocus) &&
+                (
+                    <Text
+                        style={styles.placeholder}
+                        onPress={handleOnFocus}
+                    >
+                        {placeholderText}
+                    </Text>
+                )
+            }
+            {
+                (currentKeyworldList.length > 0) &&
+                currentKeyworldList.map((keyword: string, index: number) => {
+                    return (
+                        <KeywordBadge
+                            key={index + keyword}
+                            keyword={keyword}
+                            isFixed={true}
+                            onDelete={() => deleteKeyword(index)}
+                        />
+                    );
+                })
+            }
+            {
+                (currentKeyworldList.length < maxNumber) &&
+                (isKeywordInputOnFocus) &&
+                <KeywordBadge
+                    keyword={newKeyword}
+                    isFixed={false}
+                    value={newKeyword}
+                    onChange={setNewKeyword}
+                    handleAddKeyword={handleAddKeyword}
+                />
+            }
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        minHeight: 40,
+        backgroundColor: theme.colors.achromatic05,
+        borderRadius: 5,
+        paddingHorizontal: 12,
+        paddingVertical: 7,
+        gap: 8
+    },
+    placeholder: {
+        color: theme.colors.achromatic03,
+        fontSize: theme.fontSizes.body1,
+        fontWeight: '400'
+    },
+    badgeContainer: {
+        backgroundColor: theme.colors.white,
+        borderRadius: 5,
+        paddingVertical: 4,
+        paddingHorizontal: 8,
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    badgeText: {
+        fontSize: theme.fontSizes.body2,
+        fontWeight: '400',
+        color: theme.colors.black,
+        marginRight: 10
+    },
+    badgeTextInput: {
+        fontSize: theme.fontSizes.body2,
+        fontWeight: '400',
+        color: theme.colors.black,
+        width: 'auto',
+        padding: 0,
+        margin: 0,
+    },
+    plusButtonContainer: {
+        width: 16,
+        height: 16,
+        borderRadius: 2,
+        borderWidth: 2,
+        borderColor: theme.colors.point,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: theme.colors.white
+    }
+});
+
+export default KeywordInput;
