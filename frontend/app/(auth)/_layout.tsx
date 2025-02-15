3d162bd14611f209a2c79939b26985d2f629a2d4
sbigstar0310 <sbigstar0930@gmail.com>
2025-02-16 00:56:16 +0900

    설정 페이지 로그아웃 구현

--- frontend/app/(auth)/_layout.tsx
+++ frontend/app/(auth)/_layout.tsx
@@ -1,19 +1,20 @@
-import React, {useEffect} from "react";
-import {router, Stack} from "expo-router";
+import React, { useEffect } from "react";
+import { router, Stack } from "expo-router";
+import { useAuthStore } from "@/store/authStore";
 
 export default function AuthLayout() {
     useEffect(() => {
         // TODO: Verify authentication
-        const isAuthenticated = true;
+        const isAuthenticated = useAuthStore.getState().isLoggedIn;
         if (!isAuthenticated) router.replace("/login");
-    }, []);
+    }, [useAuthStore.getState().isLoggedIn]);
 
     return (
         <>
             {/* (auth) group is assumed as a screen by main Stack navigation router */}
-            <Stack.Screen options={{headerShown: false}}/>
+            <Stack.Screen options={{ headerShown: false }} />
             {/* (auth) group needs a separate Stack navigation router for screens inside (e.g. home, search...) */}
-            <Stack screenOptions={{headerShown: false}}/>
+            <Stack screenOptions={{ headerShown: false }} />
         </>
     );
 }
--- frontend/components/MySettingSideModal.tsx
+++ frontend/components/MySettingSideModal.tsx
@@ -1,28 +1,35 @@
-import React, { useEffect, useState } from 'react';
-import SideModal from './SideModal';
-import { Image, StyleSheet, Text, Touchable, TouchableOpacity, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { useAuthStore } from '@/store/authStore';
+import React, { useEffect, useState } from "react";
+import SideModal from "./SideModal";
+import {
+    Image,
+    StyleSheet,
+    Text,
+    Touchable,
+    TouchableOpacity,
+    View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useAuthStore } from "@/store/authStore";
 import DefaultImage from "@/assets/DefaultProfile.svg";
 import RightArrow from "@/assets/RightArrow.svg";
-import theme from '@/shared/styles/theme';
-import { router } from 'expo-router';
+import theme from "@/shared/styles/theme";
+import { router } from "expo-router";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 interface MySettingSideModalProps {
     visible: boolean;
     onClose: () => void;
-};
+}
 
 const MySettingSideModal: React.FC<MySettingSideModalProps> = ({
     visible,
-    onClose
+    onClose,
 }) => {
-    
     interface UserInfo {
         image?: string | null;
         user_name?: string;
     }
-    
+
     const [userInfo, setUserInfo] = useState<UserInfo>({});
     const [userId, setUserId] = useState<number | null>(null);
 
@@ -37,7 +44,7 @@ const MySettingSideModal: React.FC<MySettingSideModalProps> = ({
                 target_user_id_string: userId,
             },
         });
-    }
+    };
 
     const handleLikedPosts = () => {
         if (!userId) {
@@ -71,7 +78,7 @@ const MySettingSideModal: React.FC<MySettingSideModalProps> = ({
                 return;
             }
         } catch (error) {
-            console.error('Error fetching my info:', error);
+            console.error("Error fetching my info:", error);
         }
     };
 
@@ -91,57 +98,42 @@ const MySettingSideModal: React.FC<MySettingSideModalProps> = ({
                 <SafeAreaView
                     style={{
                         flex: 1,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'flex-start',
-                        alignItems: 'center',
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "flex-start",
+                        alignItems: "center",
                         padding: 0,
                         paddingTop: 16,
                     }}
                 >
                     {/** profile information */}
-                    <View
-                        style={styles.myProfileContainer}
-                    >
-                        {
-                            (userInfo?.image) ? (
-                                <Image
-                                    source={{ uri: userInfo?.image }}
-                                    style={styles.profileImage}
-                                />
-                            ) : (
-                                <DefaultImage
-                                    width={36}
-                                    height={36}
-                                />
-                            )
-                        }
-                        <Text
-                            style={styles.name}
-                        >
-                            {userInfo?.user_name}
-                        </Text>
+                    <View style={styles.myProfileContainer}>
+                        {userInfo?.image ? (
+                            <Image
+                                source={{ uri: userInfo?.image }}
+                                style={styles.profileImage}
+                            />
+                        ) : (
+                            <DefaultImage width={36} height={36} />
+                        )}
+                        <Text style={styles.name}>{userInfo?.user_name}</Text>
                         <TouchableOpacity
                             onPress={() => {
-                                console.log('Do something');
+                                console.log("Do something");
                             }}
                             style={{
-                                marginLeft: 'auto'
+                                marginLeft: "auto",
                             }}
                         >
                             <RightArrow />
                         </TouchableOpacity>
                     </View>
-                    <View
-                        style={styles.settingItemContainer}
-                    >
+                    <View style={styles.settingItemContainer}>
                         <TouchableOpacity
                             style={styles.settingItem}
                             onPress={handleSubscribedProjects}
                         >
-                            <Text
-                                style={styles.settingTitleText}
-                            >
+                            <Text style={styles.settingTitleText}>
                                 {"소식 받기 한 프로젝트"}
                             </Text>
                         </TouchableOpacity>
@@ -149,9 +141,7 @@ const MySettingSideModal: React.FC<MySettingSideModalProps> = ({
                             style={styles.settingItem}
                             onPress={handleLikedPosts}
                         >
-                            <Text
-                                style={styles.settingTitleText}
-                            >
+                            <Text style={styles.settingTitleText}>
                                 {"좋아요 한 게시물"}
                             </Text>
                         </TouchableOpacity>
@@ -159,45 +149,39 @@ const MySettingSideModal: React.FC<MySettingSideModalProps> = ({
                             style={styles.settingItem}
                             onPress={handleMyOneChone}
                         >
-                            <Text
-                                style={styles.settingTitleText}
-                            >
+                            <Text style={styles.settingTitleText}>
                                 {"1촌 신청 현황"}
                             </Text>
                         </TouchableOpacity>
                         <TouchableOpacity
                             style={styles.settingItem}
                             onPress={() => {
-                                console.log('Do something');
+                                console.log("Do something");
                             }}
                         >
-                            <Text
-                                style={styles.settingTitleText}
-                            >
+                            <Text style={styles.settingTitleText}>
                                 {"팀블 서비스 안내"}
                             </Text>
                         </TouchableOpacity>
                         <TouchableOpacity
                             style={styles.settingItem}
                             onPress={() => {
-                                console.log('Do something');
+                                console.log("Do something");
                             }}
                         >
-                            <Text
-                                style={styles.settingTitleText}
-                            >
+                            <Text style={styles.settingTitleText}>
                                 {"설정"}
                             </Text>
                         </TouchableOpacity>
                         <TouchableOpacity
                             style={styles.settingItem}
                             onPress={() => {
-                                console.log('Do something');
+                                AsyncStorage.clear(); // 저장된 토큰 정보 제거
+                                useAuthStore.getState().logout(); // 로그아웃 처리
+                                router.replace("/login"); // 로그인 페이지로 리디렉션
                             }}
                         >
-                            <Text
-                                style={styles.settingTitleText}
-                            >
+                            <Text style={styles.settingTitleText}>
                                 {"로그아웃"}
                             </Text>
                         </TouchableOpacity>
@@ -210,51 +194,51 @@ const MySettingSideModal: React.FC<MySettingSideModalProps> = ({
 
 const styles = StyleSheet.create({
     myProfileContainer: {
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'flex-start',
-        alignItems: 'center',
+        width: "100%",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "flex-start",
+        alignItems: "center",
         paddingVertical: 16,
         gap: 16,
         borderBottomWidth: 2,
         borderBottomColor: theme.colors.achromatic04,
     },
-    profileImage : {
+    profileImage: {
         width: 36,
         height: 36,
         borderRadius: 18,
         borderWidth: 1,
         borderColor: theme.colors.point,
     },
-    name : {
+    name: {
         fontSize: theme.fontSizes.body1,
         color: theme.colors.black,
-        fontWeight: 600
+        fontWeight: 600,
     },
-    settingItemContainer : {
+    settingItemContainer: {
         flex: 1,
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'flex-start',
-        alignItems: 'center',
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        alignItems: "center",
         paddingVertical: 10,
     },
-    settingItem : {
-        width: '100%',
+    settingItem: {
+        width: "100%",
         paddingVertical: 10,
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'flex-start',
-        alignItems: 'center',
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "flex-start",
+        alignItems: "center",
         paddingHorizontal: 5,
     },
-    settingTitleText :{
+    settingTitleText: {
         fontSize: theme.fontSizes.body1,
         color: theme.colors.black,
-        fontWeight: 400
-    }
+        fontWeight: 400,
+    },
 });
 
 export default MySettingSideModal;
