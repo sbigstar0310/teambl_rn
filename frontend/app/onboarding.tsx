1951e61b65f578fcdd85e4e64bcb66fd597ea6d8
Gyuwon Lee <gyuwon@kaist.ac.kr>
2025-02-21 12:31:05 +0900

    onboarding 최신화

--- frontend/app/onboarding.tsx
+++ frontend/app/onboarding.tsx
@@ -1,4 +1,3 @@
-import { Button, StyleSheet, Text, View } from "react-native";
 import { router } from "expo-router";
 import { sharedStyles } from "@/app/_layout";
 import PrimeButton from "@/components/PrimeButton";
@@ -7,28 +6,22 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 const Slogan = styled.Text`
     color: #000;
-    font-family: Pretendard;
+    font-family: PretendardSemiBold;
     font-size: 26px;
-    font-weight: 600;
-    letter-spacing: -0.624px;
-    margin-bottom: 32px;
+    margin-bottom: 10px;
 `;
 
 const SemiTitle = styled.Text`
     color: #121212;
-    font-family: Pretendard;
-    font-size: 16px;
-    font-weight: 400;
-    letter-spacing: -0.384px;
-    margin-bottom: 12px;
+    font-family: PretendardRegular;
+    font-size: 15px;
+    margin-bottom: 10px;
 `;
 
 const Description = styled.Text`
     color: #595959;
-    font-family: Pretendard;
-    font-size: 14px;
-    font-weight: 400;
-    letter-spacing: -0.266px;
+    font-family: PretendardRegular;
+    font-size: 13px;
 `;
 
 const Container = styled(SafeAreaView)`
@@ -37,7 +30,6 @@ const Container = styled(SafeAreaView)`
 `;
 
 const TopContainer = styled.View`
-    padding-horizontal: 16px;
     margin-top: 126px;
 `;
 
@@ -57,14 +49,19 @@ const OnBoardingScreen = () => {
             style={[sharedStyles.container, sharedStyles.horizontalPadding]}
         >
             <TopContainer>
-                <Slogan>팀원 찾기의 새로운 기준, 팀블!</Slogan>
+                <Slogan>
+                    신뢰 기반의{"\n"}
+                    프로젝트 네트워크, 팀블!
+                </Slogan>
 
-                <SemiTitle>팀블에 오신 것을 환영합니다!</SemiTitle>
+                <SemiTitle>
+                    진행 중인 다양한 프로젝트를 살펴보고,{"\n"}
+                    관심있는 프로젝트를 응원하며 소통을 시작해보세요!
+                </SemiTitle>
 
                 {/* Body Text */}
                 <Description>
-                    팀블은 지인 네트워크를 통해 신뢰할 수 있는 팀원을 찾는
-                    플랫폼으로, 가입은 기존 회원의 초대 링크로만 가능합니다.
+                    문의하기: info@teambl.net
                 </Description>
             </TopContainer>
 
@@ -81,54 +78,5 @@ const OnBoardingScreen = () => {
         </Container>
     );
 };
-
-const styles = StyleSheet.create({
-    // Title 스타일
-    title: {
-        width: "100%",
-        textAlign: "left",
-        color: "#000000",
-        fontFamily: "Pretendard",
-        fontSize: 26,
-        fontWeight: "600",
-        letterSpacing: -0.38,
-    },
-    // Semi Title 스타일
-    semiTitle: {
-        width: "100%",
-        textAlign: "left",
-        color: "#121212",
-        fontFamily: "Pretendard",
-        fontSize: 16,
-        fontWeight: "400",
-        letterSpacing: -0.38,
-    },
-    // Body 텍스트 스타일
-    bodyText: {
-        width: "100%",
-        textAlign: "left",
-        color: "#595959",
-        fontFamily: "Pretendard",
-        fontSize: 14,
-        fontWeight: "300",
-        letterSpacing: -0.38,
-    },
-    // 여백 설정
-    horizontalPadding32: {
-        paddingHorizontal: 28,
-    },
-    marginTop126: {
-        marginTop: 126,
-    },
-    marginBottom32: {
-        marginBottom: 32,
-    },
-    marginBottom12: {
-        marginBottom: 12,
-    },
-    marginBottom312: {
-        marginBottom: 312,
-    },
-});
 
 export default OnBoardingScreen;
