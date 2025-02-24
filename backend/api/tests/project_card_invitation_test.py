6861b7a895aa4fa12ddf188c7904446a24a71386
sbigstar0310 <sbigstar0930@gmail.com>
2025-02-24 20:23:34 +0900

    [fix:backend] ProjectCardInvitationResponseView에 테스트 코드 추가, 응답 형식 변경

--- backend/api/tests/project_card_invitation_test.py
+++ backend/api/tests/project_card_invitation_test.py
@@ -7,12 +7,55 @@ from .utils import create_post_with_images, create_user_with_profile
 from ..models import (
     CustomUser,
     Friend,
+    Notification,
     Post,
     PostImage,
     ProjectCard,
     ProjectCardInvitation,
 )
 from django.core.files.uploadedfile import SimpleUploadedFile
+
+
+class ProjectCardInvitationCreateView(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+        self.url = reverse("project-card-invitation-create")  # Dynamic URL mapping
+
+        # Create a test user
+        self.testuser01 = create_user_with_profile(
+            email="test@email.com", password="test", user_name="testuser1"
+        )
+        self.testuser02 = create_user_with_profile(
+            email="testuser02@email.com", password="test", user_name="testuser2"
+        )
+
+        # Create Project Cards
+        self.project_card1 = ProjectCard.objects.create(
+            title="Test Project Card 1",
+            creator=self.testuser01,
+            description="This is a test project card 1.",
+            start_date="2022-12-31",
+            end_date="2023-01-01",
+        )
+
+        # Authenticate the user
+        self.client.force_authenticate(user=self.testuser01)
+
+    def test_create_project_card_invitation(self):
+        """
+        Ensure we can create a new project card invitation object.
+        """
+        url = reverse("project-card-invitation-create")
+
+        data = {
+            "project_card": self.project_card1.id,
+            "invitee": self.testuser02.id,
+        }
+
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(ProjectCardInvitation.objects.count(), 1)
 
 
 class ProjectCardInvitationCurrentListView(TestCase):
@@ -134,3 +177,102 @@ class ProjectCardInvitationDeleteView(TestCase):
                 id=self.project_card_invitation1.id
             ).exists()
         )
+
+
+class ProjectCardInvitationResponseView(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+        # Create a test user
+        self.testuser01 = create_user_with_profile(
+            email="test@email.com", password="test", user_name="testuser1"
+        )
+        self.testuser02 = create_user_with_profile(
+            email="testuser02@email.com", password="test", user_name="testuser2"
+        )
+
+        # Create Project Cards
+        self.project_card1 = ProjectCard.objects.create(
+            title="Test Project Card 1",
+            creator=self.testuser01,
+            description="This is a test project card 1.",
+            start_date="2022-12-31",
+            end_date="2023-01-01",
+        )
+
+        # Create Project Card Invitations
+        self.project_card_invitation1 = ProjectCardInvitation.objects.create(
+            project_card=self.project_card1,
+            inviter=self.testuser01,
+            invitee=self.testuser02,
+        )
+
+        # Authenticate the user
+        self.client.force_authenticate(user=self.testuser02)
+
+    def test_response_project_card_invitation_accept(self):
+        """
+        Ensure we can do accept response to a project card invitation.
+        """
+        url = reverse(
+            "project-card-invitation-response",
+            kwargs={"pk": self.project_card_invitation1.id},
+        )
+        data = {
+            "status": "accepted",
+        }
+
+        response = self.client.patch(url, data, format="json")
+
+        # Check the response
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.data["status"],
+            "accepted",
+        )
+        self.assertEqual(
+            ProjectCardInvitation.objects.get(
+                id=self.project_card_invitation1.id
+            ).status,
+            "accepted",
+        )
+
+        # Check the project card accepted users added
+        self.assertTrue(
+            ProjectCard.objects.get(id=self.project_card1.id)
+            .accepted_users.filter(id=self.testuser02.id)
+            .exists()
+        )
+
+        # Check Notification
+        self.assertTrue(Notification.objects.filter(user=self.testuser01).exists())
+
+    def test_response_project_card_invitation_reject(self):
+        """
+        Ensure we can do reject response to a project card invitation.
+        """
+        url = reverse(
+            "project-card-invitation-response",
+            kwargs={"pk": self.project_card_invitation1.id},
+        )
+        data = {
+            "status": "rejected",
+        }
+
+        response = self.client.patch(url, data, format="json")
+
+        # Check the response
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.data["status"],
+            "rejected",
+        )
+        self.assertEqual(
+            ProjectCardInvitation.objects.get(
+                id=self.project_card_invitation1.id
+            ).status,
+            "rejected",
+        )
+
+        # Check Notification
+        self.assertTrue(Notification.objects.filter(user=self.testuser01).exists())
